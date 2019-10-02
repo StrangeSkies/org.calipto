8b7d63df2ae6e56087fb5280fc98f2024a6eaf66
Elias N Vasylenko <eliasvasylenko@gmail.com>
2019-10-02 18:43:14 +0100

    new invoke node

--- nodes/src/main/java/org/strum/node/InvokeNode.java
+++ nodes/src/main/java/org/strum/node/InvokeNode.java
@@ -1,14 +1,53 @@
 package org.strum.node;
 
-import java.util.Arrays;
-
-import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerAsserts;
-import com.oracle.truffle.api.dsl.UnsupportedSpecializationException;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.instrumentation.StandardTags;
+import com.oracle.truffle.api.instrumentation.Tag;
+import com.oracle.truffle.api.interop.ArityException;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
-import com.oracle.truffle.api.nodes.Node;
-import com.oracle.truffle.api.nodes.UnexpectedResultException;
-import com.oracle.truffle.api.source.SourceSection;
 
-public abstract class InvokeNode extends StrumNode {}
+public abstract class InvokeNode extends ExpressionNode {
+  @Child
+  private ExpressionNode functionNode;
+  @Children
+  private final ExpressionNode[] argumentNodes;
+  @Child
+  private InteropLibrary library;
+
+  public InvokeNode(ExpressionNode functionNode, ExpressionNode[] argumentNodes) {
+    this.functionNode = functionNode;
+    this.argumentNodes = argumentNodes;
+    this.library = InteropLibrary.getFactory().createDispatched(3);
+  }
+
+  @ExplodeLoop
+  @Override
+  public Object executeGeneric(VirtualFrame frame) {
+    Object function = functionNode.executeGeneric(frame);
+
+    CompilerAsserts.compilationConstant(argumentNodes.length);
+
+    Object[] argumentValues = new Object[argumentNodes.length];
+    for (int i = 0; i < argumentNodes.length; i++) {
+      argumentValues[i] = argumentNodes[i].executeGeneric(frame);
+    }
+
+    try {
+      return library.execute(function, argumentValues);
+    } catch (ArityException | UnsupportedTypeException | UnsupportedMessageException e) {
+      // throw 
+    }
+  }
+
+  @Override
+  public boolean hasTag(Class<? extends Tag> tag) {
+    if (tag == StandardTags.CallTag.class) {
+      return true;
+    }
+    return super.hasTag(tag);
+  }
+}
